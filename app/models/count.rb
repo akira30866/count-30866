fcf64b1bd015857f69faa2c448cf00aab2acc72a
akira30866 <a.nishijimayou@gmail.com>
2020-11-16 16:10:49 +0900

    model間のアソシエーション設定

--- app/models/count.rb
+++ app/models/count.rb
@@ -1,2 +1,11 @@
 class Count < ApplicationRecord
+  # validation
+  validates :name, presence :true 
+  with_options numericality: { other_than: 1 } do
+    validate :category_id
+    validate :release_id
+  end
+  # Association
+  has_many :details
+  belongs_to :user 
 end
