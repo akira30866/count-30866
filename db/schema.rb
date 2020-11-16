5959bad4d8ea8e7a71b41e21d405728dcc742d58
akira30866 <a.nishijimayou@gmail.com>
2020-11-16 15:57:02 +0900

    countsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_091751) do
+ActiveRecord::Schema.define(version: 2020_11_16_064820) do
+
+  create_table "counts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.string "remark"
+    t.integer "category_id", null: false
+    t.integer "release_id", null: false
+    t.integer "trials"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_counts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_11_13_091751) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "counts", "users"
 end
