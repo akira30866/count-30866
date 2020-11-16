9d147fe3bbf19124d7cf87495ab31cdbb5e803bc
akira30866 <a.nishijimayou@gmail.com>
2020-11-16 17:03:53 +0900

    nested_formの準備
    これ以降nested_formを用いた実装を行う

--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -3,8 +3,14 @@ class CountsController < ApplicationController
   end
 
   def new
+    @count = Count.new
   end
 
   def create
+  end
+
+  private
+  def count_params
+    params.require(:count).permit(:image, :name, :remark, :category_id, :release_id)
   end
 end
--- app/models/count.rb
+++ app/models/count.rb
@@ -7,11 +7,12 @@ class Count < ApplicationRecord
     validates :image
   end    
   with_options numericality: { other_than: 1 } do
-    validate :category_id
-    validate :release_id
+    validates :category_id
+    validates :release_id
   end
   # Association
   has_many :details
+  accepts_nested_attributes_for :details, allow_destroy: true
   belongs_to :user 
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
