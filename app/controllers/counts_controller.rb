fcfb701b322fe8c4c272f8f779534ecf53402fe9
akira30866 <a.nishijimayou@gmail.com>
2020-11-16 20:39:17 +0900

    createアクションの設定

--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -8,10 +8,16 @@ class CountsController < ApplicationController
   end
 
   def create
+    @count = Count.new(count_params)
+    if @count.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def count_params
-    params.require(:count).permit(:image, :name, :remark, :category_id, :release_id)
+    params.require(:count).permit(:image, :name, :remark, :category_id, :release_id, details_attributes: [:count_id, :title, :number, :probability, :_destroy]).merge(user_id: current_user.id)
   end
 end
