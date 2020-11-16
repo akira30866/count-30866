88cb8d32598efe7500fe45c64ee80f13f945644c
akira30866 <a.nishijimayou@gmail.com>
2020-11-16 16:42:28 +0900

    countモデルの編集
    :imageのアソシエーションとバリデーションを設定

--- app/models/count.rb
+++ app/models/count.rb
@@ -4,6 +4,7 @@ class Count < ApplicationRecord
     validates :name
     validates :category
     validates :release
+    validates :image
   end    
   with_options numericality: { other_than: 1 } do
     validate :category_id
@@ -12,6 +13,7 @@ class Count < ApplicationRecord
   # Association
   has_many :details
   belongs_to :user 
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :release
