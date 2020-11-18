a86554de172993d9aed0944f656d603a843da3d8
akira30866 <a.nishijimayou@gmail.com>
2020-11-18 16:59:42 +0900

    コントローラーの設定
    showアクションの設定

--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -1,5 +1,6 @@
 class CountsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_count, only: [:show]
   
   def index
     @counts = Count.includes(:user).order("created_at")
@@ -19,8 +20,16 @@ class CountsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
   def count_params
     params.require(:count).permit(:image, :name, :remark, :category_id, :release_id, details_attributes: [:id, :count_id, :title, :number, :probability, :_destroy]).merge(user_id: current_user.id)
+  end
+
+  def find_count
+    @count = Count.find(params[:id])
+    @details = @count.details.build
   end
 end
